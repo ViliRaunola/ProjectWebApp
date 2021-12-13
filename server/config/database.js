820a1505542ad31c8263fb29ec26ee0af85fa538
Vili Raunola <raunolavili@gmail.com>
2021-12-13 19:12:34 +0200

    Comment to database connection

--- server/config/database.js
+++ server/config/database.js
@@ -1,3 +1,3 @@
 module.exports = {
-    database: 'mongodb+srv://admin:admin@testicluster.sr1mh.mongodb.net/WebAppProject?retryWrites=true&w=majority'
+    database: 'mongodb+srv://admin:admin@testicluster.sr1mh.mongodb.net/WebAppProject?retryWrites=true&w=majority' //Change this if you want to connect to a local database
 }
